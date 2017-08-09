15fcec1e067c1b17c7f4ae1eae00229c6ed5774d
DimaProkop <dionp95@mail.ru>
2017-08-09 17:34:17 +0300

    add skeleton cli with tests server

---
+++ src/bitbucket.org/instinctools/gluten/cli/tests_server/runner.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	pb "../proto_service"
+	"google.golang.org/grpc/reflection"
+)
+
+var address string = ":3000"
+
+type server struct{}
+
+func (s *server) SendConfig(ctx context.Context, in *pb.ParamsRequest) (*pb.ReplyMessage, error) {
+	log.Println("Request body: %v", in.Body)
+	return &pb.ReplyMessage{Message: "Good day " + "sir."}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterProtoServiceServer(s, &server{})
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
