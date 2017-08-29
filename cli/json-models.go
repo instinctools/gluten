d5bacf1abc315f636f72cf77e77eaef75c2c1d86
Sined <dzianis.kharytonau@instinctools.ru>
2017-08-29 16:25:33 +0300

    subSteps  added

--- cli/json-models.go
+++ cli/json-models.go
@@ -1,21 +1,22 @@
 package main
 
 type Project struct {
-	Name      string      `json:"Name"`
+	Name      string         `json:"Name"`
 	Scenarios []TestScenario `json:"Scenarios"`
 }
 
 type TestScenario struct {
-	Name      string     `json:"Name"`
+	Name  string     `json:"Name"`
 	Cases []TestCase `json:"Cases"`
 }
 
 type TestCase struct {
-	Name      string     `json:"Name"`
+	Name  string     `json:"Name"`
 	Steps []TestStep `json:"Steps"`
 }
 
 type TestStep struct {
-	Type   string   `json:"type"`
+	Type       string            `json:"type"`
 	Parameters map[string]string `json:"Parameters"`
+	SubSteps   []TestStep        `json:"SubSteps"`
 }
